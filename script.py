faae72a84a0e8bcd191ed7b9c8c1c1551e82a1c9
Stefan Birsan <83671730+StefanBirsan@users.noreply.github.com>
2024-01-17 19:00:49 +0200

    Update script.py

--- script.py
+++ script.py
@@ -9,3 +9,27 @@ def remove_odd_digits(number):
     filtered_digits = [digit for digit in num_str if int(digit) % 2 == 0]
     result_number = int(''.join(filtered_digits))
     return result_number
+
+
+def process(data):
+    result = []
+    p = 1
+    for x in data:
+        if x!= 0:
+            counter = 2
+            for i in range(2,x):
+                if x % i == 0:
+                    counter += 1
+            if counter == 2:
+                p = p*x
+            if x != 1:
+                result.append(counter)
+            else:
+                result.append(1)
+        else:
+            result.append(0)
+    if p == 1:
+        result.append(0)
+    else:
+        result.append(p)
+    return result
